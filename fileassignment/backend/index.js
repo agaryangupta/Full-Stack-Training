f68394dbcf221b942d2d25763dbc47c91c1b7544
Aryan Gupta <agaryangupta123@gmail.com>
2026-02-09 12:37:49 +0530

    backend

--- fileassignment/backend/index.js
+++ fileassignment/backend/index.js
@@ -8,8 +8,31 @@ const PORT = 5000;
 app.use(cors());
 app.use(express.json());
 
+// Home test
 app.get("/", (req, res) => {
   res.send("Backend is working");
+});
+
+// FS CREATE / WRITE
+app.post("/write", (req, res) => {
+  const { content } = req.body;
+
+  fs.writeFile("data.txt", content, (err) => {
+    if (err) {
+      return res.status(500).send("Error writing file");
+    }
+    res.send("File written successfully");
+  });
+});
+
+// FS READ
+app.get("/read", (req, res) => {
+  fs.readFile("data.txt", "utf-8", (err, data) => {
+    if (err) {
+      return res.status(500).send("Error reading file");
+    }
+    res.send(data);
+  });
 });
 
 app.listen(PORT, () => {
