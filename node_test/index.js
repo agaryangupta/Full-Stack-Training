33a9edd53f39d2425971566e3e9e4ceca02e7359
Aryan Gupta <agaryangupta123@gmail.com>
2026-02-11 09:59:54 +0530

    complete

--- node_test/index.js
+++ node_test/index.js
@@ -78,9 +78,12 @@
 // fs.unlinkSync("./a1.txt");
 
 const os = require('os');
-console.log('playform:', os.platform());
-console.log('architecture:', os.arch());
-console.log('info:', os.userInfo());
-console.log('version:', os.version());
-console.log('cpu:', os.cpus());
-console.log('free memory:', os.freemem());
+// console.log('playform:', os.platform());
+// console.log('architecture:', os.arch());
+// console.log('info:', os.userInfo());
+// console.log('version:', os.version());
+// console.log('cpu:', os.cpus());
+// console.log('free memory:', os.freemem());
+// console.log('total memory:', os.totalmem());
+console.log('uptime:', os.uptime());
+console.log('home directory:', os.homedir());
