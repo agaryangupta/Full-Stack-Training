953d7e198815b6b415227f9cdefa8addbf116ac5
Aryan Gupta <agaryangupta123@gmail.com>
2026-02-23 12:53:01 +0530

    express

--- express/app.js
+++ express/app.js
@@ -2,6 +2,11 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
+app.get('/',(req,res)=>{
+    res.send('Hello World');
+});
+
 app.listen(port,()=>{
-    console.log(`Server is running on port ${port}`);
+console.log(`Server is running on port http://localhost:${port}`);
 })
+
