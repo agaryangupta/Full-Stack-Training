7b49f89f9310ca1da6a82fbdf5b19eb690616eac
Aryan Gupta <agaryangupta123@gmail.com>
2026-02-23 12:55:38 +0530

    express

--- express/app.js
+++ express/app.js
@@ -6,6 +6,10 @@ app.get('/',(req,res)=>{
     res.send('Hello World');
 });
 
+app.get('/about',(req,res)=>{
+    res.send('About Us');
+});
+
 app.listen(port,()=>{
 console.log(`Server is running on port http://localhost:${port}`);
 })
