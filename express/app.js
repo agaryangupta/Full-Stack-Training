98f09711c6e8447f5b0541cb697c259ad97211a0
Aryan Gupta <agaryangupta123@gmail.com>
2026-02-23 13:02:57 +0530

    express

--- express/app.js
+++ express/app.js
@@ -7,7 +7,10 @@ app.get('/',(req,res)=>{
 });
 
 app.get('/about',(req,res)=>{
-    res.send('About Us');
+    res.send(`
+        <h1>About Us </h1>
+        <img src="https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d" width="300" height="200"/> 
+        `);
 });
 
 app.listen(port,()=>{
