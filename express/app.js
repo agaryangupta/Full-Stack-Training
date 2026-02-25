a0f4093685157b9b125e5d251e66d78c2c073d86
Aryan Gupta <agaryangupta123@gmail.com>
2026-02-25 09:54:30 +0530

    express

--- express/app.js
+++ express/app.js
@@ -1,31 +1,31 @@
 const express = require('express');
-const multer  = require('multer')
-const path = require('path');
+// const multer  = require('multer');
+// const path = require('path');
 const app = express();
 const port = 8000;
 
-const upload = multer({ dest: 'uploads/' });
+// const upload = multer({ dest: 'uploads/' });
 
 app.get('/',(req,res)=>{
     res.send('Hello World');
 });
 
-app.get('/img', (req, res) => {
-    res.send(`
-        <h2>Upload Image</h2>
-        <form action="/img" method="POST" enctype="multipart/form-data">
-            <input type="file" name="image" />
-            <button type="submit">Upload</button>
-        </form>
-    `);
-});
+// app.get('/img', (req, res) => {
+//     res.send(`
+//         <h2>Upload Image</h2>
+//         <form action="/img" method="POST" enctype="multipart/form-data">
+//             <input type="file" name="image" />
+//             <button type="submit">Upload</button>
+//         </form>
+//     `);
+// });
 
-app.post('/img', upload.single('image'), (req, res) => {
-    if (!req.file) {
-        return res.send("No file uploaded");
-    }
-    res.send("Image uploaded successfully!");
-});
+// app.post('/img', upload.single('image'), (req, res) => {
+//     if (!req.file) {
+//         return res.send("No file uploaded");
+//     }
+//     res.send("Image uploaded successfully!");
+// });
 
 // app.get('/about',(req,res)=>{
 //     res.send(`
@@ -33,6 +33,14 @@ app.post('/img', upload.single('image'), (req, res) => {
 //         <img src="https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d" width="300" height="200"/> 
 //     `);
 // });
+
+const student = [{
+    id:1,
+    name: "Aryan",
+    age: 21,
+    course: "B.Tech"
+}]
+
 
 app.listen(port,()=>{
 console.log(`Server is running on port http://localhost:${port}`);
