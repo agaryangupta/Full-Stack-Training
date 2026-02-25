1d2f2816f6a81e887c5f54360fc74f3b83dfdad3
Aryan Gupta <agaryangupta123@gmail.com>
2026-02-25 09:23:04 +0530

    express

--- express/app.js
+++ express/app.js
@@ -4,20 +4,36 @@ const path = require('path');
 const app = express();
 const port = 8000;
 
+const upload = multer({ dest: 'uploads/' });
+
 app.get('/',(req,res)=>{
     res.send('Hello World');
 });
 
-app.get('/about',(req,res)=>{
+app.get('/img', (req, res) => {
     res.send(`
-        <h1>About Us </h1>
-        <img src="https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d" width="300" height="200"/> 
-        `);
+        <h2>Upload Image</h2>
+        <form action="/img" method="POST" enctype="multipart/form-data">
+            <input type="file" name="image" />
+            <button type="submit">Upload</button>
+        </form>
+    `);
 });
 
-app.get('/img',()=>{
-    
-})
+app.post('/img', upload.single('image'), (req, res) => {
+    if (!req.file) {
+        return res.send("No file uploaded");
+    }
+
+    res.send("Image uploaded successfully!");
+});
+
+// app.get('/about',(req,res)=>{
+//     res.send(`
+//         <h1>About Us </h1>
+//         <img src="https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d" width="300" height="200"/> 
+//     `);
+// });
 
 app.listen(port,()=>{
 console.log(`Server is running on port http://localhost:${port}`);
